b36971f4d3171af580930bf67d75afa8e2571905
Ricardo Henrique Vieira Tomba <ricardohenriquevt@gmail.com>
2025-04-14 09:45:36 -0300

    Making MVC

---
+++ service/conexao.php
@@ -0,0 +1,32 @@
+<?php
+    
+    class usePDO 
+    {
+        private $servername = "localhost";
+        private $username = "root";
+        private $password = "";
+        private $dbname = "db";
+        private $instance;
+    
+    }
+    
+    function getInstance()
+    {
+        if(empty($this->instance)){
+            $this->instance = $this->connection();
+        }
+        return $this->instance;
+    }
+    
+    private function connection()
+    {
+        try {
+            $conn = new PDO("mysql:host=$this->severname;dbname=$this->dbname", $this->username, $this->password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            // echo "Connection failed: " . $e->getMessage() . "<br>";
+            die("Connection failed: " . $e->getMessage() . "<br>");
+        }
+    }
+?>
